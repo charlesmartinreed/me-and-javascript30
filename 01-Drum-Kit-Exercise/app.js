b85b22d7354d25138992a46cd10545d97cefaa71
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-12 14:21:18 -0500

    Rewrite for clarity.

--- 01-Drum-Kit-Exercise/app.js
+++ 01-Drum-Kit-Exercise/app.js
@@ -1,32 +1,23 @@
+window.addEventListener('keydown', playSound);
+
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => {
+	key.addEventListener('transitionend', removeTransition)
+})
+
 function playSound(e) {
-		// what key code was pressed - e.keycode?
-		// is there an audio element on the page that has this keycode
-		const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	if(!audio) return;
 
-		// grab reference to the key that was actually tapped
-		const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	audio.currentTime = 0;
+	audio.play();
 
-		// rewind to start of element to allow rapid retrigger
-		audio.currentTime = 0;
-		audio.play();
+	key.classList.add('playing');
 
-		// add our "pressed button" styling
-		key.classList.add('playing');
 }
 
-	function removeTransition(e) {
-		if(e.propertyName !== 'transform') return;
-		// class is removed when transitionend occurs, which, OK, we're playing with spacetime here
-		this.classList.remove('playing');
-	}
-
-	// transition complete? reverse to original state by listening for transition
-		const keys = document.querySelectorAll('.key');
-		keys.forEach((key) => {
-			key.addEventListener('transitionend', removeTransition);
-		})
-
-	// // no audio associated with the keyCode? return out of the function
-	// if(!audio) { return };
-
-window.addEventListener('keydown', playSound);
+function removeTransition(e) {
+	if(e.propertyName !== 'transform') return;
+	this.classList.remove('playing');
+}
