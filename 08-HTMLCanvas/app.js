e1c5873e83b3b999788baf4340408b5be3a0bb26
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-14 17:55:36 -0500

    Complete Exercise 9.

--- 08-HTMLCanvas/app.js
+++ 08-HTMLCanvas/app.js
@@ -11,11 +11,17 @@ const canvasHeight = window.innerHeight;
 // configuring our drawing tools
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round'; /* corner shape for stroke paths */
-ctx.lineCap = 'round;' /* shape used at the end of open subpaths, when stroked. Think of this as the brush shape */
+ctx.lineCap = 'round'; /* shape used at the end of open subpaths, when stroked. Think of this as the brush shape */
+ctx.lineWidth = 100;
+
+// think of this as as Photoshop style blend mode
+ctx.globalCompositeOperation = 'multiply';
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(e) {
 	// moved when mouse is moved atop the canvas
@@ -24,6 +30,8 @@ function draw(e) {
 	console.log(e);
 
 	// user mouse is down, let's draw!
+
+	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 	ctx.beginPath();
 	ctx.moveTo(lastX, lastY);
 	ctx.lineTo(e.offsetX, e.offsetY);
@@ -33,7 +41,18 @@ function draw(e) {
 	[lastX, lastY] = [e.offsetX, e.offsetY];
 	// lastX = e.offsetX;
 	// lastY = e.offsetY;
+	hue++; /* since we're using hsl, we can control the hue programmatically */
+	if (hue >= 360) { hue = 0 };
 
+	// flip the direction, which decides whether lineWidth is growing or shirnking
+	if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+		direction = !direction;
+	}
+	if (direction) {
+		ctx.lineWidth++;
+	} else {
+		ctx.lineWidth--;
+	}
 }
 
 // only draw when cursor is pressed down
