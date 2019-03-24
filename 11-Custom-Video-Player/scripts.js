7cb817f9948a561ade1594b3758f6f1e659418dd
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-23 22:14:58 -0500

    Add ability to change volume and playback speed with range controls. Add video seeking by clicking the scrub bar.

--- 11-Custom-Video-Player/scripts.js
+++ 11-Custom-Video-Player/scripts.js
@@ -33,12 +33,46 @@ function skip() {
 	video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+	// console.log(this.value);
+	// console.log(this.name);
+	// switch (this.name) {
+	// 	case 'volume':
+	// 	video.volume = parseFloat(this.value);
+	// 	break;
+	// 	case 'playbackRate':
+	// 	video.playbackRate = parseFloat(this.value);
+	// }
+
+	// alternate, ternary method
+	video[this.name] = this.value;
+}
+
+function handleProgress() {
+	// basically, we'll just be updating the flex-basis of the progress bar to indicate how far along in the video we are
+	const percent = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+	// listen for a click on the progress, divide the click location (offsetX) from the total width of progress bar (offsetWidth), use that to set the current time of the video
+
+	const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+	video.currentTime = scrubTime;
+
+	// console.log(e);
+}
+
 // Connect event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+// alternatively, the 'progress' event emitter allows the same functionality. either way, each time the video fires off the "timeudpate event", we change the flex basis for our progress bar
+video.addEventListener('timeupdate', handleProgress);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('input', handleRangeUpdate));
 
+progress.addEventListener('click', scrub);
 
 toggle.addEventListener('click', togglePlay);
